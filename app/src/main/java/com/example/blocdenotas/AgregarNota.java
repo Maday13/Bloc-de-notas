7eeb1c3cd818ed2e03d2b3f5507394a342829a3c
ECR23 <eicr23@outlook.com>
2020-11-03 13:19:12 -0600

    Hola

--- app/src/main/java/com/example/blocdenotas/AgregarNota.java
+++ app/src/main/java/com/example/blocdenotas/AgregarNota.java
@@ -1,16 +1,29 @@
 package com.example.blocdenotas;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AgregarNota extends AppCompatActivity
 {
     Button Add;
-    String type;
+    EditText TITLE, CONTENT;
+    String type, getTitle;
+    private static final int SALIR = Menu.FIRST;
+    AdaptadorBD DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,15 +32,78 @@ public class AgregarNota extends AppCompatActivity
         setContentView(R.layout.agregar_nota);
 
         Add = (Button)findViewById(R.id.button_Add);
-
+        TITLE = (EditText)findViewById(R.id.editText_Titulo);
+        CONTENT =(EditText)findViewById(R.id.editText_Nota);
         Bundle bundle = this.getIntent().getExtras();
-
+        String content;
+        getTitle = bundle.getString("title");
+        content = bundle.getString("content");
         type = bundle.getString("type");
 
         if(type.equals("add"))
-        {
             Add.setText("Add nota");
+        else{
+            if(type.equals("edit")){
+                TITLE.setText(getTitle);
+                CONTENT.setText(content);
+                Add.setText("UPDATE NOTA");
+            }
+        }
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addUpdateNotes();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(1, SALIR, 0, R.string.menu_salir);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+       int id = item.getItemId();
+       switch (id){
+           case SALIR:
+               CookieSyncManager.createInstance(this);
+               CookieManager cookieManager = CookieManager.getInstance();
+               cookieManager.removeAllCookie();
+               Intent intent = new Intent(AgregarNota.this,
+                       MainActivity.class);
+               intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK
+               | Intent.FLAG_ACTIVITY_NEW_TASK);
+               startActivity(intent);
+               return true;
+
+           default:
+               return super.onOptionsItemSelected(item);
+       }
+    }
+
+    private void addUpdateNotes(){
+        DB = new AdaptadorBD(this);
+        String title, content, msj;
+        title = TITLE.getText().toString();
+        content = CONTENT.getText().toString();
+        if(type.equals("add")){
+            if(title.equals("")){
+                msj = "INGRESE UN TITULO";
+                TITLE.requestFocus();
+                Mensaje(msj);
+            }
         }
 
+    }
+
+    public void Mensaje (String msj){
+        Toast toast = Toast.makeText(this, msj, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_VERTICAL,0,0);
+        toast.show();
     }
 }
--- app/src/main/java/com/example/blocdenotas/MainActivity.java
+++ app/src/main/java/com/example/blocdenotas/MainActivity.java
@@ -73,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
 
             case DELETE:
                 return true;
-
             case EXIST:
                 finish();
                 return true;
